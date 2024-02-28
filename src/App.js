96beb81e78d35396601a7d49c9944c764987b891
mavlenkulovachynara <mavlenkulovachynara@gmail.com>
2024-02-29 01:58:10 +0600

    craze mind

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import MyRoutes from "./routes/MyRoutes";
 import Footer from "./components/Footer/Footer";
-import AuthContextProvider from "./context/AuthContextProvider";
-import PostContextPrivder from "./context/PostContextPrivder";
 
 const App = () => {
   return (
     <>
-      <AuthContextProvider>
-        <PostContextPrivder>
           <Navbar />
           <MyRoutes />
           <Footer />
-        </PostContextPrivder>
-      </AuthContextProvider>
     </>
   );
 };
--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import NotFound from "../components/NotFound/NotFound";
 import { Route, Routes } from "react-router-dom";
 import Main from "../components/Main/Main";
-import Login from "../components/Auth/Login";
-import Register from "../components/Auth/Register";
 import About from "../components/about/About";
 import AboutUs from "../components/about/Payment";
 import ProductList from "../components/product/ProductList";
@@ -13,8 +11,6 @@ import ProductItem from "../components/product/ProductItem";
 const PUBLIC_ROUTES = [
   { id: 1, link: "*", element: <NotFound /> },
   { id: 2, link: "/", element: <Main /> },
-  { id: 3, link: "/login", element: <Login /> },
-  { id: 4, link: "/register", element: <Register /> },
   { id: 5, link: "/about", element: <About/> },
   { id: 6, link: "/payments", element: <AboutUs /> },
   { id: 7, link: "/productlist", element: <ProductList /> },
